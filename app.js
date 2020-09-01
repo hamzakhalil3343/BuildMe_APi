ef91ad6d611a14842aa2f0ac652874dc8c008715
Hamza <hamza.khalil3343@gmail.com>
2020-09-01 15:34:43 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,64 @@
+var express = require('express');
+var path = require('path');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var config = require('./config');
+var passport = require('passport');
+
+
+//db connection
+const url = config.mongoUrl;
+const mongoose = require('mongoose');
+
+
+const connect = mongoose.connect(url);
+
+connect.then(() => {
+    console.log("Connected correctly to server");
+}, (err) => { console.log(err); });
+var app = express();
+//passport code 
+app.use(passport.initialize());
+app.use(passport.session());
+//Route files
+var index = require('./routes/index');
+var users = require('./routes/users');
+
+
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Routes to use
+app.use('/', index);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handler
+app.use(function(err, req, res) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
